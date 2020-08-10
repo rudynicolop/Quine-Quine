e5850448167dc677e8f8f67dd0cde1800eb446d0
rnp39 <rnp39@cornell.edu>
2020-08-10 13:17:28 -0700

    quines are confusing

---
+++ quine.py
@@ -0,0 +1,5 @@
+# f = lambda x : x + "\n print(f(" + x + "))"
+#
+# print(f('lambda x : x + "\n" + x'))
+
+print ((lambda x : 'print (' + x + '(' + x + '))')("(lambda x : 'print (' + x + '(' + x + '))'"))
